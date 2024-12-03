c89065d1cf81f10d25df954939d4b41aa5022678
yashsiwacha <yashsiwacha@outlook.com>
2024-12-03 20:51:29 +0530

    Updates

--- Data/Data.java
+++ Data/Data.java
@@ -1,4 +1,4 @@
-package Java;
+package Java.Data;
 
 public class Data {
     public static void main(String[] args) {
--- Hello/Hello.java
+++ Hello/Hello.java
@@ -1,4 +1,4 @@
-package Java;
+package Java.Hello;
 public class Hello{
     public static void main(String args[]){
         System.out.println("Hello World!!!!");
